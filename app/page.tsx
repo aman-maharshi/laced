e50d2105a51ff3056fd7681e7d2d58b03cd8978e
aman-maharshi <amanm4harshi@gmail.com>
2025-08-23 23:52:27 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <section className="flex-start flex-col p-4">
+      <div className="text-3xl font-bold">Laced</div>
+    </section>
+  )
+}
+
+export default Home
